14168b4a9816c126884cfafd7ca935b6d7bb3ab9
PrashantJha4762 <jhaprashantkumar73@gmail.com>
2026-08-18 11:58:58 +0530

    added status codes

--- HotelService/src/controllers/hotel.controller.ts
+++ HotelService/src/controllers/hotel.controller.ts
@@ -1,9 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 import { CreateHotelService, GetALlHotelService, GetByIDService } from "../services/hotel.services";
+import { StatusCodes } from "http-status-codes";
 
 export async function CreateHotelHandler(req:Request,res:Response,next:NextFunction){
     const hotelresponse= await CreateHotelService(req.body);
-    res.status(201).json({
+    res.status(StatusCodes.CREATED).json({
         message:"Hotel created succesfully",
         data:hotelresponse,
         success:true
@@ -11,7 +12,7 @@ export async function CreateHotelHandler(req:Request,res:Response,next:NextFunct
 }
 export async function GetHoetlByidhandler(req:Request,res:Response,next:NextFunction){
     const hotelresponse= await GetByIDService(Number(req.params.id));
-    res.status(200).json({
+    res.status(StatusCodes.OK).json({
         message:"Hotel found succesfully",
         data:hotelresponse,
         success:true
@@ -19,7 +20,7 @@ export async function GetHoetlByidhandler(req:Request,res:Response,next:NextFunc
 }
 export async function GetAllHotelsHandler(req:Request,res:Response,next:NextFunction){
     const hotelresponse= await GetALlHotelService();
-    res.status(200).json({
+    res.status(StatusCodes.OK).json({
         message:"Hotels found succesfully",
         data:hotelresponse,
         success:true
--- HotelService/src/controllers/pinghandler.ts
+++ HotelService/src/controllers/pinghandler.ts
@@ -1,8 +1,9 @@
 import type { NextFunction, Request, Response } from "express"
 import { logger } from "../config/logger.config";
+import { StatusCodes } from "http-status-codes";
 export const pinghandler=async(req:Request,res:Response,next:NextFunction)=>{
         logger.info("Ping request received");
-        res.status(200).json({
+        res.status(StatusCodes.OK).json({
                 message:"pong",
         })
 }
