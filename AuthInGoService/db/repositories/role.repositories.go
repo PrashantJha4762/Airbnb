876ccb1422f7f13502cbe783538a992786ad739b
PrashantJha4762 <jhaprashantkumar73@gmail.com>
2026-09-12 17:57:41 +0530

    added fn for CreateRole

--- AuthInGoService/db/repositories/role.repositories.go
+++ AuthInGoService/db/repositories/role.repositories.go
@@ -3,11 +3,14 @@ package db
 import (
 	"AuthInGoService/models"
 	"database/sql"
+	"fmt"
 )
 
 type RoleRepository interface {
 	GetRoleById(id int) (*models.Role,error)
 	GetRoleByName(name string) (*models.Role,error)
+	GetAllRoles() ([]*models.Role,error)
+	CreateRole(name, description string) error
 }
 type RoleRepositoryImpl struct {
 	db *sql.DB
@@ -38,6 +41,42 @@ func (r *RoleRepositoryImpl) GetRoleByName(name string) (*models.Role,error){
 		return nil, err
 	}
 	return role, nil
+}
+func (r *RoleRepositoryImpl) GetAllRoles() ([]*models.Role,error){
+	query:="Select id, name,description, created_at, updated_at from roles"
+	rows,err:=r.db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	roles:=[]*models.Role{}
+	for rows.Next(){
+		role:=models.Role{}
+		err:=rows.Scan(&role.Id,&role.Name,&role.Description,&role.CreatedAt,&role.UpdatedAt)
+		if err != nil {
+			return nil, err
+		}
+		roles = append(roles, &role)
+	}
+	return roles, nil
+}
+func (r *RoleRepositoryImpl) CreateRole(name, description string)error{
+	query:="Insert into roles(name,description) values(?,?)"
+	result,err:=r.db.Exec(query,name,description)
+	if err != nil {
+		return  err
+	}
+	rowsaffected,err:=result.RowsAffected()
+	if err != nil {
+		return  err
+	}
+	if rowsaffected==0{
+		return fmt.Errorf("Role not created")
+	}
+	if rowsaffected>0{
+		fmt.Println("Role created successfully")
+	}
+	return nil
 }
 func NewRoleRepository(db *sql.DB) RoleRepository {
 	return &RoleRepositoryImpl{
