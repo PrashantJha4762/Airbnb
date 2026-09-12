1f9442cf64e669e31c2742a361a4240a32b61b7c
PrashantJha4762 <jhaprashantkumar73@gmail.com>
2026-09-12 17:47:54 +0530

    added fn for getting role id

--- AuthInGoService/db/repositories/role.repositories.go
+++ AuthInGoService/db/repositories/role.repositories.go
@@ -1,12 +1,29 @@
 package db
 
-import "database/sql"
+import (
+	"AuthInGoService/models"
+	"database/sql"
+)
 
 type RoleRepository interface {
 	GetRoleById(id int) (*models.Role,error)
 }
 type RoleRepositoryImpl struct {
 	db *sql.DB
+}
+
+func (r *RoleRepositoryImpl) GetRoleById(id int) (*models.Role,error){
+	query:="Select id, name,description, created_at, updated_at from roles where id=?"
+
+	row:=r.db.QueryRow(query,id)
+
+	role:=&models.Role{}
+
+	err:=row.Scan(&role.Id,&role.Name,&role.Description,&role.CreatedAt,&role.UpdatedAt)
+	if err != nil {
+		return nil, err
+	}
+	return role, nil
 }
 func NewRoleRepository(db *sql.DB) RoleRepository {
 	return &RoleRepositoryImpl{
