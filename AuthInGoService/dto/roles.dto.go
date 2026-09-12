423a4ba8772f8214b41c06c084874ee4c8a80222
PrashantJha4762 <jhaprashantkumar73@gmail.com>
2026-09-12 17:41:34 +0530

    added dto for permission

--- AuthInGoService/dto/roles.dto.go
+++ AuthInGoService/dto/roles.dto.go
@@ -7,3 +7,12 @@ type Role struct{
 	CreatedAt string
 	UpdatedAt string
 }
+type Permission struct{
+	Id int 
+	Name string
+	Description string
+	Resource string
+	Action string
+	CreatedAt string
+	UpdatedAt string
+}
