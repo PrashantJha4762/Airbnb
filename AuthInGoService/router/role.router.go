450679072ea50c68f06a512587447198f4de744f
PrashantJha4762 <jhaprashantkumar73@gmail.com>
2026-09-14 15:47:27 +0530

    added routing layer for the controller fns

--- AuthInGoService/router/role.router.go
+++ AuthInGoService/router/role.router.go
@@ -19,4 +19,8 @@ func NewRoleRouter(_rolecontroller *controllers.RoleController) Router {
 func (rr *RoleRouter) Register(r *chi.Mux) {
 	r.Get("/roles/{id}",rr.rolecontroller.GetRoleById);
 	r.Get("/roles",rr.rolecontroller.GetAllRoles);
+	r.Post("/roles",rr.rolecontroller.CreateRole);
+	r.Delete("/roles/{id}",rr.rolecontroller.DeleteRoleById);
+	r.Post("/roles/assign",rr.rolecontroller.AssignRoleToUser);
+	r.Post("/roles/remove",rr.rolecontroller.RemoveRoleFromUser);
 }
